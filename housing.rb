dec0183df9418ef9ad23a7207b5f2a3cd6cedc3d
chanhpark <chanhyun.park1211@gmail.com>
2014-12-17 19:06:57 -0500

    reading from csv file and saving to csv file

--- housing.rb
+++ housing.rb
@@ -1,18 +1,31 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
+require 'pry'
 
-url = "http://www.zillow.com/homes/07650_rb"
-html = open(url)
-scrape = Nokogiri::HTML(html)
-puts scrape.css("#search-title").text
 
-n = 0
-scrape.css(".property-info").each do |info|
-  if info.css(".routable") != nil
-    size = info.css(".property-data").text
-    price = info.css(".price-large").text
-    address = info.css(".routable").text
+  zipcodes = []
+
+CSV.foreach("zipcode.csv", headers: true) do |row|{ zipcode: row['zipcode'] }
+  row.each { |key,value| zipcodes << value }
+end
+
+zipcodes.each do |code|
+
+  url = "http://www.zillow.com/homes/#{code}_rb"
+
+  html = open(url)
+  scrape = Nokogiri::HTML(html)
+
+  scrape.css(".property-info").each do |info|
+    if info.css(".routable") != nil
+      size = info.css(".property-data").text
+      price = info.css(".price-large").text
+      address = info.css(".routable").text
+    end
+    CSV.open("zillow_info.csv", 'a') do |csv|
+      csv << [address, size, price]
+    end
   end
-    puts "#{n+=1}) #{address} || #{size} for #{price} "
 end
